5eb6a367ff127f148c4d7778d040dde8918a4a33
Md Nashir Kazi <99824485+kazi35@users.noreply.github.com>
2023-11-22 01:48:30 +0600

    Add files via upload

---
+++ Devloper.java
@@ -0,0 +1,14 @@
+public class Devloper {
+    void dev()
+    {
+        System.out.println("Mithun Kumar das");
+        System.out.println("213-35-796");
+        System.out.println("b.sc in SWE At Diu");
+    }
+
+    void help()
+    {
+        System.out.println("help.public@gmail.com");
+        System.out.println("Number_ 888");
+    }
+}
---
+++ Login_info.java
@@ -0,0 +1,20 @@
+public class Login_info {
+    private String user_name = "Mithun";
+    private String pass = "796";
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
